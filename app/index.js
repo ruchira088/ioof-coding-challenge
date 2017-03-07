2f34d53dc2da6f5d7bc4d21edb1fcc74ebacd2b6
Ruchira (Richie) Jayasekara <ruchira088@gmail.com>
2017-03-08 03:06:43 +1100

    Minor refactor

--- app/index.js
+++ app/index.js
@@ -3,21 +3,30 @@ const path = require("path")
 const {getDateDifference} = require("./date")
 const {DATE_DELIMITER, FILE_ENCODING, INPUT_FILE_NAME} = require("./constants")
 
+/**
+ * Splits the start date and the end date
+ */
 const splitDates = dates => {
     const [startDate, endDate] = dates.trim().split(DATE_DELIMITER).map(date => date.trim())
     return {startDate, endDate}
 }
 
+/**
+ * Fetches the dates from the command line
+ */
 const fetchDatesFromCommandLine = args => {
     const [,,dates] = args
 
     if(dates != null) {
-        return splitDates(dates)
+        return dates
     } else {
         return null
     }
 }
 
+/**
+ * Fetches the dates from the input file
+ */
 const fetchDatesFromFile = filePath => new Promise((resolve, reject) => {
     fs.readFile(filePath, FILE_ENCODING, (error, data) => {
         if(error) {
@@ -28,6 +37,10 @@ const fetchDatesFromFile = filePath => new Promise((resolve, reject) => {
     })
 })
 
+/**
+ * Fetches the dates. It will initially fetch the dates from the command line. If no
+ * dates passes in the command line, then it will fetch the dates from the input file.
+ */
 const fetchDates = () => {
     const commandLineDates = fetchDatesFromCommandLine(process.argv)
 
@@ -35,12 +48,13 @@ const fetchDates = () => {
         return Promise.resolve(commandLineDates)
     } else {
         const filePath = path.join(path.dirname(__dirname), INPUT_FILE_NAME)
-        return fetchDatesFromFile(filePath).then(splitDates)
+        return fetchDatesFromFile(filePath)
     }
 }
 
 fetchDates()
-    .then(({startDate, endDate}) => {
+    .then(dates => {
+        const {startDate, endDate} = splitDates(dates)
         console.log(`${startDate}, ${endDate}, ${getDateDifference(startDate, endDate)}`)
     })
     .catch(error => {
